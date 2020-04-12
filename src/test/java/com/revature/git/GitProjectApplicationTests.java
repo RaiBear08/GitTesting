cd5c7c52ff2e4190349c0cd05a495ff3b9147e79
RaiBear08 <61160598+RaiBear08@users.noreply.github.com>
2020-04-12 07:15:30 -0700

    first commit

---
+++ src/test/java/com/revature/git/GitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
